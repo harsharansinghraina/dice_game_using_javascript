ba2e21043c000ac24179eb5cbae5b0fbb8bde727
notfakeharsh <73037452+notfakeharsh@users.noreply.github.com>
2020-10-23 23:58:09 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+
+var randomNumber1 = Math.floor(Math.random()*6+1);
+var randomNumber2 = Math.floor(Math.random()*6+1);
+
+randomDice1 = "images/dice" + randomNumber1 + ".png";
+
+randomDice2 = "images/dice" + randomNumber2 + ".png";
+
+document.querySelector(".img1").getAttribute("src");
+
+document.querySelector(".img1").setAttribute("src", randomDice1);
+
+document.querySelector(".img2").getAttribute("src");
+
+document.querySelector(".img2").setAttribute("src", randomDice2);
+
+if (randomNumber1 > randomNumber2) {
+  document.querySelector("h2").innerText= "🚩Player 1 wins";
+}
+
+if (randomNumber1 === randomNumber2) {
+  document.querySelector("h2").innerText="Draw!";
+}
+
+if (randomNumber1 < randomNumber2) {
+  document.querySelector("h2").innerText="Player 2 wins🚩";
+}
